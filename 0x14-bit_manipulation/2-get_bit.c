d8308bd34b6593259bd383269cbfddf1831f848e
root <ramiromohorami@gmail.com>
2023-10-26 18:42:49 -0700

    bit value

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,12 +7,15 @@
  * Return: index if true -1 if error
  */
 
-int get_bit(unsigned long int n, unsigned long index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	int value;
+		int bit_val;
 
-	if (index > 63)
-		return (-1);
-	value = (n >> index) & 1;
-	return (value);
+		if (index > 63)
+			return (-1);
+
+		bit_val = (n >> index) & 1;
+
+		return (bit_val);
 }
+
