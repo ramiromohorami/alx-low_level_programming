75dee40a260325c85d7112420d214f47286daf41
root <ramiromohorami@gmail.com>
2023-10-30 12:13:33 -0700

    reading and writing string

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
@@ -9,24 +10,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
+	char *buf;
 	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
 
-	if (filename == NULL)
-		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buffer, letters);
-	if (t == -1)
-		return (0);
-	w = write(STDOUT_FILENO, buffer, t);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	if (w != t || w == -1)
-		return (0);
 	free(buf);
 	close(fd);
 	return (w);
