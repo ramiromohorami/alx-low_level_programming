a2bfa63297ec1e0cdca945d8c9d6e42fdbe123e6
root <ramiromohorami@gmail.com>
2023-12-07 15:51:39 -0800

    linked1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "linkedlist.h"
+/**
+ * print_dlistint - prints double linked list
+ * @h: address of the head ode
+ * Return: size
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h>n);
+		h = h->next;
+		i++;
+	}
+	return (i):
+}
+
+
