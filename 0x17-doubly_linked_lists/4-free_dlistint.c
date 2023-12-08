701f053866d9d0eed64f63dc9a75a57b9c949dd1
root <ramiromohorami@gmail.com>
2023-12-08 12:37:32 -0800

    task4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint -  free a double list
+ * @head: pointer to the head
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+	}
+}
