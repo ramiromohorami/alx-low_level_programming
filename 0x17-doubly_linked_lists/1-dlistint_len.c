55c8888fde89b8192f32dc519f30a8d1f9f5e125
root <ramiromohorami@gmail.com>
2023-12-08 10:37:24 -0800

    task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -  return length of the list
+ * @h: address of the head of list
+ * Return: size
+ */
+size_t dlist_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
