b9948ec63a5a6ba8a7318180f4f55447b063f690
root <ramiromohorami@gmail.com>
2023-12-21 16:42:09 -0800

    task6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,7 +10,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *next;
 
-	if (ht == NULL || ht->array == NULL || hr->size == 0)
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
@@ -18,13 +18,13 @@ void hash_table_delete(hash_table_t *ht)
 		{
 			next = ht->array[i]->next;
 			free(ht->array[i]->key);
-			free(ht->array[i]->value;
+			free(ht->array[i]->value);
 			free(ht->array[i]);
 			ht->array[i] = next;
 		}
 
 	}
-	free(ht->aray);
+	free(ht->array);
 	ht->array = NULL;
 	ht->size = 0;
 	free(ht);
